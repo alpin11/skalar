117ebd25357cc3b4239464016dc80c313567ce5a
Jonas <jonas@jmp.blue>
2023-04-06 12:50:24 +0200

    ~ return raw data if decoding runs into limit error

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,19 @@
-FROM rust:1.67 as builder
+FROM rust:1.68 as builder
 WORKDIR /usr/src/image-scaling
 COPY . .
 RUN cargo install --path .
 
 FROM debian:bullseye-slim
-RUN apt-get update && apt-get install openssl ca-certificates
+RUN apt-get update && apt-get -y install openssl ca-certificates curl \
+  && rm -rfv /var/lib/apt/lists/*
+
 RUN update-ca-certificates
 COPY --from=builder /usr/local/cargo/bin/image-scaling /usr/local/bin/image-scaling
+
+RUN adduser --disabled-password --gecos '' image-scaling
+USER image-scaling
+ENV USER=image-scaling
+
+RUN chown -R image-scaling .
+
 CMD ["image-scaling"]
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use axum::{
     routing::get,
     Router,
 };
-use image::ImageFormat;
+use image::{ImageFormat, io::{Reader, Limits}, guess_format, ImageError};
 use request_context::RequestContext;
 use reqwest::{header, StatusCode};
 use std::{
@@ -47,12 +47,12 @@ async fn handle(
     }
 
     // get image format
-    let extension_string = &ctx.format.unwrap_or("webp".into());
-    let format = ImageFormat::from_extension(extension_string).unwrap_or(ImageFormat::WebP);
+    let extension_string = &ctx.format.clone().unwrap_or("webp".into());
+    let requested_format = ImageFormat::from_extension(extension_string).unwrap_or(ImageFormat::WebP);
     let mime_type = mime_guess::from_ext(extension_string).first_or("image/webp".parse().unwrap());
 
     // download imgage
-    let res = reqwest::get(ctx.url)
+    let res = reqwest::get(&ctx.url)
         .await
         .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Error Fetching Image".to_string()))?;
     if res.error_for_status_ref().is_err() {
@@ -63,8 +63,47 @@ async fn handle(
         .bytes()
         .await
         .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Error Fetching Image".to_string()))?;
-    let image = image::load_from_memory(&bytes)
-        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Invalid Image".to_string()))?;
+
+    // determine image format
+    let fetched_format = guess_format(&bytes)
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Unable to determine Image format".to_string()))?;
+
+    // decode image
+    let mut decoder = Reader::new(Cursor::new(&bytes));
+    decoder.set_format(fetched_format);
+    let mut limits = Limits::no_limits();
+    // set allocator limit to 1gb
+    limits.max_alloc = Some(1024 * 1024 * 1024);
+    decoder.limits(limits);
+
+
+    let image = decoder.decode();
+
+    if image.is_err() {
+        let err = image.unwrap_err();
+        return match err {
+            ImageError::Limits(e) => {
+                // since not all decoders use the correct allocator limits, specifically the png one
+                // as a temprorary workarround until proper limits are used here
+                // https://github.com/image-rs/image-png/blob/2f53fc40b91a5e1d0ad1801b746c04a7fe1d8603/src/decoder/stream.rs#L751
+                // we return the fetched data directly instead of trying to convert it
+                // TODO: remove temporary workarround
+                println!("{:?} on {:?} for format {:?}", e, &ctx.url, &fetched_format);
+                let mime_type = mime_guess::from_path(&ctx.url).first_or("image/png".parse().unwrap());
+                Ok((
+                    [(header::CONTENT_TYPE, mime_type.to_string())],
+                    [(
+                        header::CACHE_CONTROL,
+                        format!("max-age={}", ctx.cache_max_age.unwrap_or(31536000)),
+                    )],
+                    bytes.to_vec(),
+                ))
+            },
+            _ => Err((StatusCode::INTERNAL_SERVER_ERROR, "Error decoding image".to_string()))
+        }
+    }
+
+    let image = image.unwrap();
 
     // resize
     let image = image.resize(
@@ -77,7 +116,7 @@ async fn handle(
     let image = image.to_rgba8();
     let mut buffer = BufWriter::new(Cursor::new(Vec::new()));
     image
-        .write_to(&mut buffer, format)
+        .write_to(&mut buffer, requested_format)
         .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Error Converting to requested format".to_string()))?;
     let bytes: Vec<u8> = buffer
         .into_inner()
